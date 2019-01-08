b125e6c4e08fc68bb827bf82b21ac8cb11d48314
Camron Fritz <camfritz@gmail.com>
2019-01-07 22:42:46 -0800

    Add functionality for email/password authentication through firebase.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
-const bootstrap = require('bootstrap')
-const firebase = require('firebase')
+function signInClickListener(email, password) {
+	firebase.auth().signInWithEmailAndPassword(email, password)
+	.then(function() {
+		console.log(email)
+		alert("Success!");
+	})
+	.catch(function(error) {
+		alert(error)
+	});
+}
 
-var config = {
-	  apiKey: "AIzaSyAtaDx6v3Q4yNAQVMe-z88v-64YKRTCOrQ",
-    authDomain: "cs493-14e09.firebaseapp.com",
-    databaseURL: "https://cs493-14e09.firebaseio.com",
-    projectId: "cs493-14e09",
-    storageBucket: "cs493-14e09.appspot.com",
-    messagingSenderId: "276297056112"
-};
-
-firebase.initializeApp(config);
+function signUpClickListener(email, password) {
+	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+		alert(error)
+	});
+}
